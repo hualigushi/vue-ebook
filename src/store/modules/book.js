f6875245e83990bf0f8009f836a78f827b497cc3
money <qianlingya6275@gmail.com>
2019-08-06 15:30:59 +0800

    Update book.js

--- src/store/modules/book.js
+++ src/store/modules/book.js
@@ -3,7 +3,21 @@ const book = {
     fileName: '',
     menuVisible: false,
     settingVisible: -1, // -1:不显示，0：字号设置,1:主题设置，2:进度设置，3:目录显示
-    defaultFontSize: 16
+    defaultFontSize: 16,
+    defaultFontFamily: 'Default',
+    fontFamilyVisible: false,
+    defaultTheme: 'default',
+    bookAvailable: false,
+    progress: 0,
+    section: 0,
+    isPagination: true,
+    currentBook: null, // 当前显示的电子书
+    navigation: null,
+    cover: null,
+    metadata: null,
+    paginate: '',
+    offsetY: 0,
+    isBookmark: null
   },
   mutations: {
     'SET_FILENAME': (state, fileName) => {
@@ -17,7 +31,10 @@ const book = {
     },
     'SET_DEFAULTFONTSIZE': (state, defaultFontSize) => {
         state.defaultFontSize = defaultFontSize
-    }
+    },
+    'SET_CURRENTBOOK': (state, currentBook) => {
+         state.currentBook = currentBook
+     }
   },
   actions: {
     
