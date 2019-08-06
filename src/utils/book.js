d5138fed0f66623b7f56b0cd41e8d8e80f0f9b9c
money <qianlingya6275@gmail.com>
2019-08-06 17:08:11 +0800

    Update book.js

--- src/utils/book.js
+++ src/utils/book.js
@@ -1,4 +1,4 @@
-export const FONT_SIZE_LIST = {
+export const FONT_SIZE_LIST = [
   {fontSize: 12},
    {fontSize: 14},
     {fontSize: 16},
@@ -6,4 +6,11 @@ export const FONT_SIZE_LIST = {
       {fontSize: 20},
        {fontSize: 22},
         {fontSize: 24},
-}
+]
+export const FONT_FAMILY = [
+  {font: 'Default'},
+  {font: 'Cabin'},
+  {font: 'Days One'},
+  {font: 'Montserrat'},
+  {font: 'Tangerine'}
+]
