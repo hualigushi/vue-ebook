2fffd7f192a5bdc778d0add4163c7f8325f7dcde
money <qianlingya6275@gmail.com>
2019-08-06 11:41:51 +0800

    Update book.js

--- src/store/modules/book.js
+++ src/store/modules/book.js
@@ -1,7 +1,8 @@
 const book = {
   state: {
     fileName: '',
-    menuVisible" false
+    menuVisible: false,
+    settingVisible: -1 // -1:不显示，0：字号设置,1:主题设置，2:进度设置，3:目录显示
   },
   mutations: {
     'SET_FILENAME': (state, fileName) => {
@@ -9,6 +10,9 @@ const book = {
     },
     'SET_MENUVISIBLE': (state, menuVisible) => {
        state.menuVisible = menuVisible
+    },
+    'SET_SETTINGVISIBLE': (state, settingVisible) => {
+       state.settingVisible = settingVisible
     }
   },
   actions: {
