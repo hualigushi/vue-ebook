b47a091cddddeb1bdbf76ef17d22b181227cc1c2
money <qianlingya6275@gmail.com>
2019-08-20 11:49:38 +0800

    Update StoreHome.vue

--- src/views/ebook/store/StoreHome.vue
+++ src/views/ebook/store/StoreHome.vue
@@ -1,13 +1,25 @@
 <template>
 <div class="store-home">
   <search-bar></search-bar>
+  <scroll :top="94" @onScroll="onScroll">
+    <div></div
+  </scroll>
 </div>
 <template>
 <script>
 import SearchBar from '../../components/home/SearchBar'
+import Scroll from '../../components/common/Scroll'
+import {storeHomeMixin} from '../../utils/mixin'
 export default {
+   minxins: [storeHomeMinxin],
   compoents:{
-    SearchBar
+    SearchBar,
+    Scroll
+  },
+  methods: {
+    onScroll(offsetY) {
+       this.setOffsetY(offsetY)
+    }
   }
 }
 </script>
